dcb4bb2f2fc0ede144d55bceca4b623775bcb2b1
jusmccar <justinmccartney1@yahoo.com>
2026-01-14 22:38:55 -0800

    Add loading transitions on sign up and login buttons

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -18,12 +18,15 @@ import {
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import z from "zod";
 
 export default function LoginPage() {
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
   const form = useForm({
@@ -34,19 +37,21 @@ export default function LoginPage() {
     },
   });
 
-  async function onSubmit(data: z.infer<typeof loginSchema>) {
-    await authClient.signIn.email({
-      email: data.email,
-      password: data.password,
-      fetchOptions: {
-        onSuccess: () => {
-          toast.success("Logged in successfully");
-          router.push("/");
+  function onSubmit(data: z.infer<typeof loginSchema>) {
+    startTransition(async () => {
+      await authClient.signIn.email({
+        email: data.email,
+        password: data.password,
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Logged in successfully");
+            router.push("/");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
         },
-        onError: (error) => {
-          toast.error(error.error.message);
-        },
-      },
+      });
     });
   }
 
@@ -97,7 +102,16 @@ export default function LoginPage() {
               )}
             />
 
-            <Button>Login</Button>
+            <Button disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Loader2 className="size-4 animate-spin" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                <span>Login</span>
+              )}
+            </Button>
           </FieldGroup>
         </form>
       </CardContent>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -18,12 +18,15 @@ import {
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import z from "zod";
 
 export default function SignUpPage() {
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
   const form = useForm({
@@ -35,20 +38,22 @@ export default function SignUpPage() {
     },
   });
 
-  async function onSubmit(data: z.infer<typeof signUpSchema>) {
-    await authClient.signUp.email({
-      email: data.email,
-      name: data.name,
-      password: data.password,
-      fetchOptions: {
-        onSuccess: () => {
-          toast.success("Account created successfully");
-          router.push("/");
+  function onSubmit(data: z.infer<typeof signUpSchema>) {
+    startTransition(async () => {
+      await authClient.signUp.email({
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Account created successfully");
+            router.push("/");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
         },
-        onError: (error) => {
-          toast.error(error.error.message);
-        },
-      },
+      });
     });
   }
 
@@ -117,7 +122,16 @@ export default function SignUpPage() {
               )}
             />
 
-            <Button>Sign Up</Button>
+            <Button disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Loader2 className="size-4 animate-spin" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                <span>Sign Up</span>
+              )}
+            </Button>
           </FieldGroup>
         </form>
       </CardContent>
